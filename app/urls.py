be887860f50815cd7047db9cb8f28ca1f5ea19c9
matias <matiasjose547@gmail.com>
2022-07-01 22:37:18 -0300

    deleted module app, created module sales

--- app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from app.views import category_list, category_create
-
-urlpatterns = [
-    path('category_list/', category_list),
-    path('category_create/', category_create),
-]
---
+++ sales/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+
+def teste(request):
+    return render(request, "home.html") 
+# Create your views here.
